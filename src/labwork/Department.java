38f83244c25ab2800b2de8b801a9c6e6c20dc5bf
homePC <miha.pobeda11@gmail.com>
2022-09-01 00:45:49 +0300

    labwork

--- src/labwork/Department.java
+++ src/labwork/Department.java
@@ -1,17 +1,45 @@
 package labwork;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Department {
 
     public String name;
+    public Set<Instructor> instructorSet = new HashSet();
 
     public Department(String name) {
         this.name = name;
     }
 
-    public void addInstructor(String name) {
-        Instructor newInstructor = new Instructor(name);
+    public Instructor addInstructor(String name, Department department) {
+        System.out.println();
+        Instructor instructor = new Instructor(name);
+        instructorSet.add(instructor);
+        System.out.println("На " + department.name + " появился новый преподаватель: " + instructor.name);
+
+        return instructor;
     }
 
-    public void removeInstructor(String name) {
+    public void removeInstructor(Instructor instructor, Department department) {
+        System.out.println();
+        System.out.println("С " + department.name + " был уволен преподаватель " + instructor.name);
+        instructorSet.remove(instructor);
+    }
+
+    public void getInstructor(Instructor instructor) {
+        System.out.println();
+        System.out.println(instructor.name);
+    }
+
+    public void getAllInstructor(Department department) {
+        System.out.println();
+        int i = 0;
+        System.out.println("Список преподавателей:");
+        for (Instructor instructor : instructorSet) {
+            i++;
+            System.out.println(i + ". " + instructor.name);
+        }
+        System.out.println("На " + department.name + " работает " + i + " преподавателей");
     }
 }
--- src/labwork/University.java
+++ src/labwork/University.java
@@ -1,4 +1,65 @@
 package labwork;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class University {
+
+    public String name;
+    public String address;
+    public int number;
+    public Set<Department> departmentSet = new HashSet();
+    public Set<Student> studentSet = new HashSet();
+
+    public University(String name, String address, int number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public Student addStudent(String name, int studentId) {
+        Student student = new Student(name, studentId);
+        studentSet.add(student);
+
+        return student;
+    }
+
+    public void removeStudent(Student student) {
+        studentSet.remove(student);
+    }
+
+    public void getStudent(Student student) {
+        System.out.println();
+        System.out.println("name: " + student.name + ", studentID: " + student.studentId);
+    }
+
+    public Department addDepartament(String name, University university) {
+        Department department = new Department(name);
+        departmentSet.add(department);
+        System.out.println("В университете " + university.name + " была сформирована " + department.name);
+
+        return department;
+    }
+
+    public void removeDepartment(Department department, University university) {
+        System.out.println();
+        System.out.println("В университете " + university.name + " была расформирована " + department.name);
+        departmentSet.remove(department);
+    }
+
+    public void getDepartment(Department department) {
+        System.out.println();
+        System.out.println(department.name);
+    }
+
+    public void getAllDepartment(University university) {
+        System.out.println();
+        int i = 0;
+        System.out.println("Список кафедр в университете " + university.name + ": ");
+        for (Department department : departmentSet) {
+            i++;
+            System.out.println(i + ". " + department.name);
+        }
+        System.out.println("Кафедр в университете всего: " + i);
+    }
 }
